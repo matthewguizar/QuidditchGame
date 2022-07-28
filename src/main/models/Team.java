8a3ec83c4c871bb2de2177c8a5a768598a70e1c3
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-28 08:05:13 -0700

    created team class

---
+++ src/main/models/Team.java
@@ -0,0 +1,92 @@
+package src.main.models;
+
+import java.util.Arrays;
+
+public class Team {
+    private String house;
+    private String keeper;
+    private String seeker;
+    private String[] chasers;
+    private static final String POSITION_CHASER = "chaser";
+    private static final String POSITION_SEEKER = "seeker";
+    private static final String POSITION_KEEPER = "keeper";
+    
+
+/* FREQUENTLY ASKED QUESTIONS:
+    
+Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
+  Answer: Even if the constant is final, I prefer to expose a method instead of the variable. But if you want to expose the variable, that's also correct.
+
+*/
+
+    public Team(String house, String keeper, String seeker, String[] chasers){
+        this.house = house;
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = Arrays.copyOf(chasers, chasers.length);
+    }
+
+    public Team(Team team){
+        this.house = team.house;
+        this.keeper = team.keeper;
+        this.seeker = team.seeker;
+        this.chasers = Arrays.copyOf(team.chasers, team.chasers.length);
+    }
+
+    public String getHouse() {
+        return this.house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getKeeper() {
+        return this.keeper;
+    }
+
+    public void setKeeper(String keeper) {
+        this.keeper = keeper;
+    }
+
+    public String getSeeker() {
+        return this.seeker;
+    }
+
+    public void setSeeker(String seeker) {
+        this.seeker = seeker;
+    }
+
+    public String[] getChasers() {
+        return this.chasers;
+    }
+
+    public void setChasers(String[] chasers) {
+        this.chasers = chasers;
+    }
+    
+
+     public static String getPositionChaser() {
+         return POSITION_CHASER;
+     }
+
+     public static String getPositionSeeker() {
+         return POSITION_SEEKER;
+     }
+
+     public static String getPositionKeeper() {
+         return POSITION_KEEPER;
+     }
+
+
+    @Override
+    public String toString() {
+        return 
+            "House: " + this.house + "\n" +
+            "Keeper: " + this.keeper + "\n" +         
+            "Seeker: "  + this.seeker + "\n" +         
+            "Chasers: " + Arrays.toString(this.chasers) + "\n"; 
+    }
+    
+
+}
---
+++ src/test/TeamTest.java
@@ -0,0 +1,9 @@
+package src.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TeamTest {
+    
+
+}
