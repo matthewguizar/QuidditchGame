c503f89159aed5f0b7fe87fe3c26fb4d6456bdb1
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-04 12:48:45 -0700

    added hashmap to game class

--- src/main/models/Game.java
+++ src/main/models/Game.java
@@ -1,6 +1,17 @@
 package src.main.models;
 
+import java.util.HashMap;
+
 public class Game {
+
+    private HashMap<Team, Integer> scoreboard;
+
+
+    public Game(Team home, Team away){
+        this.scoreboard = new HashMap<>();
+        scoreboard.put(new Team(home), 0);
+        scoreboard.put(new Team(away), 0);
+    }
 
     
 
